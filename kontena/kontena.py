791b5d1a0da0bdb5b471222e22db3d3cb567259e
yukaritan <yukaritan@users.noreply.github.com>
2014-08-23 16:19:02 +0200

    started work on a basic structure

--- kontena/kontena.py
+++ kontena/kontena.py
@@ -21,6 +21,3 @@ class Kontena:
             function(match)
 
 
-Kontena().handle_message("a is b")
-Kontena().handle_message("dog is cat")
-Kontena().handle_message("human watches football")  # no match for this line
---
+++ kontena/test.py
@@ -0,0 +1,5 @@
+from kontena import Kontena
+
+Kontena().handle_message("a is b")
+Kontena().handle_message("dog is cat")
+Kontena().handle_message("human watches football")  # no match for this line
